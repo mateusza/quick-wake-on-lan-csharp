ec50489f636a6da4a6e290432054444e1758407f
Mateusz Adamowski <mateusz@adamowski.pl>
2017-06-22 21:38:00 +0200

    another simplification

--- wol.cs
+++ wol.cs
@@ -6,8 +6,8 @@ using System.Reflection;
 [assembly: AssemblyTitle("Wake-On-LAN Magic Packet Generator")]
 [assembly: AssemblyProduct("Wake-On-LAN")]
 [assembly: AssemblyCopyright("© 2017 Mateusz Adamowski")]
-[assembly: AssemblyVersion("0.0.2.0")]
-[assembly: AssemblyFileVersion("0.0.2.0")]
+[assembly: AssemblyVersion("0.0.3.0")]
+[assembly: AssemblyFileVersion("0.0.3.0")]
 
 public class WakeOnLan_Tool {
 
@@ -15,15 +15,16 @@ public class WakeOnLan_Tool {
     static readonly byte[] MAC = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
     public static int Main(){
-        var bcast = new IPEndPoint( new IPAddress( 0xffffffff ), 9 );
+        if ( MAC.Length != 6 ){
+            throw new Exception("Invalid MAC length");
+        }
 
-        var magicpacket = new byte[ 17 * 6 ];
-
+        var magicpacket = new byte[ 0x11 * MAC.Length ];
         for ( int i = 0; i < magicpacket.Length; i++ )
-            magicpacket[ i ] = i < 6 ? (byte) 0xff : MAC[ i % 6 ];
+            magicpacket[ i ] = i < 6 ? (byte) 0xff : MAC[ i % MAC.Length ];
 
         var client = new UdpClient();
-        client.Send( magicpacket, magicpacket.Length, bcast );
+        client.Send( magicpacket, magicpacket.Length, new IPEndPoint( IPAddress.Broadcast, 9 ) );
 
         Console.WriteLine( "Magic packet sent" );
         return 0;
